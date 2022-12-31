0a934f48993f13ebd50cc953c0633e6046e8c850
xinsughitle <116938976+xinsughitle@users.noreply.github.com>
2022-12-31 14:48:53 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,52 @@
-basic.forever(function () {
-	
+input.onGesture(Gesture.LogoUp, function () {
+    radio.sendNumber(1)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
 })
+input.onGesture(Gesture.LogoDown, function () {
+    radio.sendNumber(2)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    radio.sendNumber(3)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    radio.sendNumber(0)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    radio.sendNumber(4)
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+})
+radio.setGroup(1)
+radio.setTransmitPower(7)
